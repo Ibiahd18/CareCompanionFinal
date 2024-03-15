750a836e44cd05b42cc868a0b3a4b5067b318b60
Amina <aminahaq05@hotmail.com>
2024-03-15 23:45:44 +0000

    tim tim is alive

--- app/src/main/java/com/example/carecompanionfinal/MainActivity.java
+++ app/src/main/java/com/example/carecompanionfinal/MainActivity.java
@@ -20,5 +20,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return true;
     }
 }
