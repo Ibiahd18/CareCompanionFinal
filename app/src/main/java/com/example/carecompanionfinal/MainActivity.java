d36c9a2da7cc9516ae70762bc3b99cf36c24f032
Amina <aminahaq05@hotmail.com>
2024-03-18 02:00:42 +0000

    hellllloooo

--- app/src/main/java/com/example/carecompanionfinal/MainActivity.java
+++ app/src/main/java/com/example/carecompanionfinal/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.carecompanionfinal;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        return true;
     }
 }
