7482f454a1dbbac85a3a59de3df9e886a6036231
Amina <aminahaq05@hotmail.com>
2024-03-20 00:08:59 +0000

    spinnnnnnnnnn

--- app/src/main/java/com/example/carecompanionfinal/Create_medical.java
+++ app/src/main/java/com/example/carecompanionfinal/Create_medical.java
@@ -1,9 +1,16 @@
 package com.example.carecompanionfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class Create_medical extends AppCompatActivity {
 
@@ -19,8 +26,18 @@ public class Create_medical extends AppCompatActivity {
                 R.array.options_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-        // Set the prompt for the Spinner
-        spinner.setPrompt("Select Blood Type");
     }
 }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_create_medical);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//    }
+//}
