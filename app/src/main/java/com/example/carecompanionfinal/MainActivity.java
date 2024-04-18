f5cc787a5ee67afb08c416f50c4546924aa32d0b
Amina <aminahaq05@hotmail.com>
2024-04-18 14:59:09 +0100

    purpleeeeeeeeeeeeeeee girafffffffffffffffff

--- app/src/main/java/com/example/carecompanionfinal/MainActivity.java
+++ app/src/main/java/com/example/carecompanionfinal/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.carecompanionfinal;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+//Variables
+    DrawerLayout drawerLayout;
+
+    ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
+//    Toolbar toolbar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        drawerLayout = findViewById(R.id.main);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.nav_view);
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
         });
 
     }
@@ -61,4 +98,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        return false;
+//    }
 }
