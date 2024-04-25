290becaf4ac99044b2323be3c8b4b2e7e62823e4
Amina <aminahaq05@hotmail.com>
2024-04-25 14:52:37 +0100

    orangeeeeeeeeeeeeee corcodilesssssssssssss

--- app/src/main/java/com/example/carecompanionfinal/Create_medical.java
+++ app/src/main/java/com/example/carecompanionfinal/Create_medical.java
@@ -60,24 +60,3 @@ public class Create_medical extends AppCompatActivity {
         startActivity(intent);
     }
 }
-
-
-
-//        spinner = findViewById(R.id.spinnerBlood);
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.blood_array, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_create_medical);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
-//}
